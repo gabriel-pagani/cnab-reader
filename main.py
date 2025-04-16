e40ebee4661e38412d96ec908204fb527594020e
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-04-16 08:22:57 -0300

    feat: adicionei uma pausa aleatória antes de monitorar os arquivos

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from src.utils.connection import close_connection
 from src.controller.cnab import Cnab
+from time import sleep
+from random import random
 
 if __name__ == "__main__":
     try:
+        sleep(random())
         cnab = Cnab()
         cnab.monitor()
     finally:
